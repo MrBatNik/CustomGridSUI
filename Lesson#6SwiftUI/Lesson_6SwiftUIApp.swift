0965b88593cd1ed140568ae7003fc5b28b12df35
Nikita <nicandreo@gmail.com>
2022-02-06 19:43:53 +0300

    Initial Commit

---
+++ Lesson#6SwiftUI/Lesson_6SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_6SwiftUIApp.swift
+//  Lesson#6SwiftUI
+//
+//  Created by Nikita Batrakov on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_6SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
